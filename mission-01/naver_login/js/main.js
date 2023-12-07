1b7adb45349bd5834af8cd44da80214bf55c1956
ryukyunghye <ryukyung123@naver.com>
2023-12-07 20:31:42 +0900

    add: 과제 요구사항 구현

--- mission-01/naver_login/js/main.js
+++ mission-01/naver_login/js/main.js
@@ -1,36 +1,48 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: 'asd@naver.com',
+  pw: 'spdlqj123!@',
+};
 
-/*
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
-
-*/
-
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(text).toLowerCase())
-}
-
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+const checkEmail = () => {
+  const userEmailInput = document.querySelector('.user-email-input');
+  const isValid = emailReg(userEmailInput.value);
 
+  if (!isValid || userEmailInput.value != user.id) {
+    userEmailInput.classList.add('is--invalid');
+    return false;
+  } else {
+    userEmailInput.classList.remove('is--invalid');
+    return true;
+  }
+};
+const checkPw = () => {
+  const userPwInput = document.querySelector('.user-password-input');
+  const isValid = pwReg(userPwInput.value);
+  if (!isValid || userPwInput.value != user.pw) {
+    userPwInput.classList.add('is--invalid');
+    return false;
+  } else {
+    userPwInput.classList.remove('is--invalid');
+    return true;
+  }
+};
+document.querySelector('.btn-login').addEventListener('click', (e) => {
+  e.preventDefault();
 
-
-
-
-
-
-
-
-
+  if (checkEmail() && checkPw()) {
+    window.location.href = './welcome.html';
+  } else {
+    return false;
+  }
+});
