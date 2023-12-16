c55d27098ba17feabd1643cdafac6ffc53a292f4
ryukyunghye <ryukyung123@naver.com>
2023-12-16 18:46:04 +0900

    docs: main.js 주석 추가

--- mission-02/client/js/main.js
+++ mission-02/client/js/main.js
@@ -2,29 +2,34 @@ import dataList from './data.js';
 
 const navigation = document.querySelector('nav ul');
 
+// body의 배경 설정
 const setBgColor = (poster) => {
   const colorList = poster.color;
   const body = document.querySelector('body');
   body.style.background = `linear-gradient(to bottom, ${colorList[0]},${colorList[1]})`;
 };
 
+// 썸네일 이미지와 대체 텍스트 설정
 const setImage = (poster) => {
   const visualImg = document.querySelector('.visual img');
   visualImg.src = poster.src;
   visualImg.alt = poster.alt;
 };
 
+// 썸네일 이름 설정
 const setNameText = (poster) => {
   const visualName = document.querySelector('h1');
   visualName.textContent = poster.name;
 };
 
+// 오디오 재생
 const setAudio = (poster) => {
   const audio = new Audio(`./assets/audio/${poster.name.toLowerCase()}.m4a`);
   audio.play();
 };
 
-const posterClick = (target) => {
+// 포스터 아이템 클릭 시 이벤트 처리
+const handlePoster = (target) => {
   const targetImg = target.querySelector('button img');
   const selectedPoster = dataList.find((data) => data.alt === targetImg.alt);
 
@@ -37,10 +42,11 @@ const posterClick = (target) => {
   setAudio(selectedPoster);
 };
 
+// 포스터 클릭 이벤트 처리
 const handleClick = (e) => {
   const targetItem = e.target.closest('li');
   if (!targetItem) return;
-  posterClick(targetItem);
+  handlePoster(targetItem);
 };
 
 navigation.addEventListener('click', handleClick);
