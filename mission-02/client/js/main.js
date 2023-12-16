0d3fc17e39b750faec8d0f75859efb935956e033
ryukyunghye <ryukyung123@naver.com>
2023-12-16 17:29:02 +0900

    feat: 과제 요구사항 구현

--- mission-02/client/js/main.js
+++ mission-02/client/js/main.js
@@ -1,24 +1,46 @@
+import dataList from './data.js';
 
-/* 
+const navigation = document.querySelector('nav ul');
 
-1. 클릭 이벤트 활성화
-2. nav 클릭시 배경 색상 변경
-3. 이미지 변경
-4. 텍스트 변경
-5. 함수 분리
+const setBgColor = (poster) => {
+  const colorList = poster.color;
+  const body = document.querySelector('body');
+  body.style.background = `linear-gradient(to bottom, ${colorList[0]},${colorList[1]})`;
+};
 
-*/
+const setImage = (poster) => {
+  const visualImg = document.querySelector('.visual img');
+  visualImg.src = poster.src;
+  visualImg.alt = poster.alt;
+};
 
+const setNameText = (poster) => {
+  const visualName = document.querySelector('h1');
+  visualName.textContent = poster.name;
+};
 
+const setAudio = (poster) => {
+  const audio = new Audio(`./assets/audio/${poster.name.toLowerCase()}.m4a`);
+  audio.play();
+};
 
+const posterClick = (target) => {
+  const targetImg = target.querySelector('button img');
+  const selectedPoster = dataList.find((data) => data.alt === targetImg.alt);
 
+  [...navigation.children].forEach((li) => li.classList.remove('is-active'));
+  target.classList.add('is-active');
 
+  setBgColor(selectedPoster);
+  setImage(targetImg);
+  setNameText(selectedPoster);
+  setAudio(selectedPoster);
+};
 
+const handleClick = (e) => {
+  const targetItem = e.target.closest('li');
+  if (!targetItem) return;
+  posterClick(targetItem);
+};
 
-
-
-
-
-
-
-
+navigation.addEventListener('click', handleClick);
