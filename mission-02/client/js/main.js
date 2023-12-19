65d435e3a54217822f7b7087810558bf90d0b480
ryukyunghye <ryukyung123@naver.com>
2023-12-19 17:42:21 +0900

    refactor: 함수 매개변수 유연하게 받도록 리팩토링

--- mission-02/client/js/main.js
+++ mission-02/client/js/main.js
@@ -2,24 +2,26 @@ import dataList from './data.js';
 
 const navigation = document.querySelector('nav ul');
 
+// target의 type이 문자열인지, DOM 요소인지 확인하는 함수
+const isString = (target) =>
+  typeof target === 'string' ? document.querySelector(target) : target;
+
 // body의 배경 설정
-const setBgColor = (poster) => {
-  const [colorA, colorB = '#000'] = poster.color;
-  const body = document.querySelector('body');
-  body.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
+const setBgColor = (target, [colorA, colorB = '#000']) => {
+  target = isString(target);
+  target.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
 };
 
 // 썸네일 이미지와 대체 텍스트 설정
-const setImage = (poster) => {
-  const visualImg = document.querySelector('.visual img');
-  visualImg.src = poster.src;
-  visualImg.alt = poster.alt;
+const setImage = (target, poster) => {
+  target = isString(target);
+  target.src = `./assets/${poster.name.toLowerCase()}.jpeg`;
+  target.alt = poster.alt;
 };
-
 // 썸네일 이름 설정
-const setNameText = (poster) => {
-  const visualName = document.querySelector('h1');
-  visualName.textContent = poster.name;
+const setNameText = (target, poster) => {
+  target = isString(target);
+  target.textContent = poster.name;
 };
 
 // 오디오 재생
@@ -31,14 +33,15 @@ const setAudio = (poster) => {
 // 포스터 아이템 클릭 시 이벤트 처리
 const handlePoster = (target) => {
   const targetImg = target.querySelector('button img');
+  const visualImg = document.querySelector('.visual img');
   const selectedPoster = dataList.find((data) => data.alt === targetImg.alt);
 
   [...navigation.children].forEach((li) => li.classList.remove('is-active'));
   target.classList.add('is-active');
 
-  setBgColor(selectedPoster);
-  setImage(targetImg);
-  setNameText(selectedPoster);
+  setBgColor('body', selectedPoster.color);
+  setImage(visualImg, selectedPoster);
+  setNameText('h1', selectedPoster);
   setAudio(selectedPoster);
 };
 
